542bf223670e1caa39f518b8d5a6bd9e5f0f907b
Kostiantyn Utiupin <37743425+centipedeee@users.noreply.github.com>
2024-12-22 23:39:09 +0200

    cleaning

--- andante-bot.py
+++ andante-bot.py
@@ -1,11 +1,9 @@
-from dotenv import load_dotenv
 import tracemalloc
 from src import parser, tgbot
 import multiprocessing
 
 if __name__ == "__main__":
     tracemalloc.start()
-    load_dotenv()
     parser_process = multiprocessing.Process(target=parser.main, args=(), daemon=True)
     tgbot_process = multiprocessing.Process(target=tgbot.main, args=(), daemon=True)
     parser_process.start()
